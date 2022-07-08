b633bb6534498a402962ee46dce5e61eb0d7f982
Elena <lenhenlady@gmail.com>
2022-07-08 10:59:55 -0700

    schema file

---
+++ server/src/schema.js
@@ -0,0 +1,24 @@
+const { gql } = require('apollo-server')
+
+const typeDefs = gql`
+type Track {
+    id: ID!
+    title: String!
+    author: Author!
+    thumbnail: String
+    length: Int
+    modulesCount: Int
+}
+
+type Author {
+    id: ID!
+    name: String!
+    photo: String
+}
+"query to get tracks for homepage grid"
+type Query {
+    tracksForHome: [Track!]!
+}
+`
+
+module.exports = typeDefs
