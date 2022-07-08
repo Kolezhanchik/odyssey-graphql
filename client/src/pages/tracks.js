79c68af25ab3331429f523544b3756944cff3880
Elena <lenhenlady@gmail.com>
2022-07-08 15:28:10 -0700

    Track added

--- client/src/pages/tracks.js
+++ client/src/pages/tracks.js
@@ -1,10 +1,9 @@
 import React from 'react';
 import { Layout } from '../components';
+import { gql } from '@apollo/client';
 
-/**
- * Tracks Page is the Catstronauts home page.
- * We display a grid of tracks fetched with useQuery with the TRACKS query
- */
+const TRACKS = gql`
+`
 const Tracks = () => {
   return <Layout grid> </Layout>;
 };
